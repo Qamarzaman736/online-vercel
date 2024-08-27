b7c995cdf5a76b32d53a51885f8d4df74a0d980f
Qamar-Zaman <qamarzaman123pt@gmail.com>
2024-08-27 04:54:38 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <image/> className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" scr="/qamar.jpg" alt="" width="384" height="512"/
+  <image/> src="/qamar.jpg" // Path to your image
+        alt="Description of the image" // Alt text for the image
+        width={500} // Desired width
+        height={300} // Desired height
+        priority // Optional: Loads the image with high priority
+      / aisy likhi
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
